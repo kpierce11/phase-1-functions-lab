e59458ce9f2006276fcd43424846adb574401435
Keira Pierce <kpierce1@samford.edu>
2024-05-18 13:53:38 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(42 - someValue);
+}
+function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue)
+    const feet = blocks * 264;
+    return feet;
+}
+function distanceTravelledInFeet(start, destination) {
+    const blocks = Math.abs(start - destination )
+    const feet = blocks * 264
+    return feet
+}
+function calculatesFarePrice(start, destination) {
+    const feet = distanceTravelledInFeet(start, destination);
+    if (feet > 2500) {
+        return 'cannot travel that far';
+    } else if (feet > 2000) {
+        return 25;
+    } else if (feet > 400) {
+        const price = (feet - 400) * 0.02;
+        return parseFloat(price.toFixed(2))
+    } else {
+        return 0;
+    }
+    
+}
